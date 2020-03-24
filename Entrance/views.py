e02927d83b63ff08d4d0adf7983f58b909117b49
ajeetexe <46785184+ajeetexe@users.noreply.github.com>
2020-03-24 15:30:55 +0530

    file comited

---
+++ Entrance/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+# Create your views here.
+
+
+class Index(TemplateView):
+    template_name = 'index.html'
+
+
+
+class Contact(TemplateView):
+    template_name = 'contact.html'
